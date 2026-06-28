4771440c8d350f994eb809fbb80e8b8efac5b574
Batool Kazmi <112828911+BatoolKazmi@users.noreply.github.com>
2026-06-28 19:50:25 -0400

    Add: sqrtx - submission-1

---
+++ Data Structures & Algorithms/sqrtx/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        l, r = 1, x
+        res = 0
+
+        while l <= r:
+            mid = (l + r) // 2
+            temp = mid * mid
+
+            if temp > x:
+                r = mid - 1
+            else:
+                l = mid + 1
+                res = max(res, mid)
+                
+        return res 
